a45c99c64fb34f8a1a9e4390e8fa0a0babde34e3
Gençay Yıldız <gyildizmail@gmail.com>
2024-05-25 00:55:54 +0300

    Fix

--- Amazon.EC2.Hosting.Example/ApplicationDbContext.cs
+++ Amazon.EC2.Hosting.Example/ApplicationDbContext.cs
@@ -11,5 +11,12 @@ namespace Amazon.EC2.Hosting.Example
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18, 4)");
+        }
     }
 }
